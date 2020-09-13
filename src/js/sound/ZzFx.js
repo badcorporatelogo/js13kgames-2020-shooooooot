112649d153acc48e4fa297cfec62e70750f7e3f6
Stephen Styrchak <badcorporatelogo@hotmail.com>
2020-09-13 10:10:33 -0700

    Fixes for Safari compatibility. (#1)
    Seems like Safari doesn't support static fields yet.
    
    It also implements the draft AudioContext API with vendor-specific 
    prefix "webkit".

--- src/js/sound/ZzFx.js
+++ src/js/sound/ZzFx.js
@@ -14,6 +14,7 @@ import { volume as zzfxV } from '../settings/volume'
 const zzfxR = 44100 // sample rate
 
 export default function zzfxInit() {
+  var AudioContext = window.AudioContext || window.webkitAudioContext;
   const zzfxX = new AudioContext()
 
   // ZzFXMicro - Zuper Zmall Zound Zynth
